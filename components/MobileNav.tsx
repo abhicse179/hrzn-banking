a9a8eb9b86a29110595ef795992c822a1ef1c218
Abhisek Behera <abhisek.bhr1998@gmail.com>
2024-08-24 00:23:58 +0530

    Add plaid link connect button in mobile nav

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -16,6 +16,7 @@ import { sidebarLinks } from "@/constants";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 import Footer from "./Footer";
+import PlaidLink from "./PlaidLink";
 
 const MobileNav = ({ user }: MobileNavProps) => {
   const pathname = usePathname();
@@ -86,7 +87,7 @@ const MobileNav = ({ user }: MobileNavProps) => {
                       </SheetClose>
                     );
                   })}
-                  USER
+                  <PlaidLink user={user} variant="ghost" />
                 </nav>
               </SheetClose>
               <Footer user={user} type="mobile" />
